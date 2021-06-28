e0c3ac7b8ac2e1a17f93485e5274686c6531fcf9
NAZIYA8 <syeda.naziya896@gmail.com>
2021-06-29 01:11:37 +0530

    added: created employee payroll service to read and write a file

--- src/main/java/model/EmployeePayrollData.java
+++ src/main/java/model/EmployeePayrollData.java
@@ -1,5 +1,54 @@
+/**
+ * ****************************************************************************
+ * Purpose: This is a POJO for Employee payroll data
+ * 			It consist of Employee id, name and salary.
+ *
+ * @author Syeda Naziya
+ * @version 1.0
+ * @since 29-06-2021
+ * ****************************************************************************
+ */
+
 package model;
 
 public class EmployeePayrollData {
+	private int id;
+	private String name;
+	private double salary;
+
+	public EmployeePayrollData(int id, String name, double salary) {
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+	@Override
+	public String toString() {
+		return "EmployeePayrollData{" + "id=" + id + ", name='" + name + '\'' + ", salary=" + salary + '}';
+	}
 
 }
--- src/main/java/service/EmployeePayrollService.java
+++ src/main/java/service/EmployeePayrollService.java
@@ -1,5 +1,63 @@
+/**
+ * ****************************************************************************
+ * Purpose: This class is implemented to read and write the employee payroll data 
+ * 			to a file.
+ *
+ * @author Syeda Naziya
+ * @version 1.0
+ * @since 29-06-2021
+ * ****************************************************************************
+ */
+
 package service;
 
-public class EmployeePayrollService {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import model.EmployeePayrollData;
 
+public class EmployeePayrollService {
+	private List<EmployeePayrollData> employeePayrollList;
+
+	public EmployeePayrollService() {
+	}
+
+	public EmployeePayrollService(List<EmployeePayrollData> employeePayrollList) {
+		this.employeePayrollList = employeePayrollList;
+	}
+
+	/**
+	 * This is the main method of the program.
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		ArrayList<EmployeePayrollData> employeePayrollList = new ArrayList<>();
+		EmployeePayrollService employeePayrollService = new EmployeePayrollService(employeePayrollList);
+		Scanner consoleInputReader = new Scanner(System.in);
+		employeePayrollService.readEmployeePayrollData(consoleInputReader);
+		employeePayrollService.writeEmployeePayrollData();
+	}
+
+	/**
+	 * This method is used to read the Employee payroll data.
+	 * 
+	 * @param consoleInputReader reads input from console
+	 */
+	private void readEmployeePayrollData(Scanner consoleInputReader) {
+		System.out.println("Enter Employee id");
+		int id = consoleInputReader.nextInt();
+		System.out.println("Enter Employee name");
+		String name = consoleInputReader.next();
+		System.out.println("Enter Employee salary");
+		double salary = consoleInputReader.nextDouble();
+		employeePayrollList.add(new EmployeePayrollData(id, name, salary));
+	}
+
+	/**
+	 * This method is used to write the employee payroll data to console
+	 */
+	private void writeEmployeePayrollData() {
+		System.out.println("\nWriting Employee Payroll Roaster to Console\n" + employeePayrollList);
+	}
 }
